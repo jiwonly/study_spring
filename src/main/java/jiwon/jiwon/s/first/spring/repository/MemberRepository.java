a63aa388aa9c13edc38492919d3b4656e9e0cab8
jiwon <ysjjw2003@gmail.com>
2025-03-03 15:55:24 +0900

    feat: add test case

---
+++ src/main/java/jiwon/jiwon/s/first/spring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package jiwon.jiwon.s.first.spring.repository;
+
+import jiwon.jiwon.s.first.spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
