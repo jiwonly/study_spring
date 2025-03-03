4dbeee9c2cc1e87840592fed2e1f5b50f3069c12
jiwon <ysjjw2003@gmail.com>
2025-03-03 15:55:24 +0900

    feat: file upload

---
+++ src/test/java/jiwon/jiwon/s/first/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package jiwon.jiwon.s.first.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
